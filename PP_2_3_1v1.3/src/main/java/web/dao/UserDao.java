a42d4c36de7f4e72959878e7bd45b5ce7cdb5d6c
darthmullog <168669499+darthmullog@users.noreply.github.com>
2024-08-06 01:45:24 +0300

    Add files via upload

---
+++ PP_2_3_1v1.3/src/main/java/web/dao/UserDao.java
@@ -0,0 +1,17 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void addUser(User user);
+
+    void updateUser(User user);
+
+    void removeUser(Long id);
+
+    User getUserById(Long id);
+
+    List<User> listUsers();
+}
---
+++ PP_2_3_1v1.3/src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,49 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    @Transactional
+    public void removeUser(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+
+    @Override
+    @Transactional
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    @Transactional
+    public List<User> listUsers() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+}
